09f358ef0e129ff06809b6c492cad3c7c3099196
philipearnshaw <philip.earnshaw@sky.uk>
2018-03-05 17:15:18 +0000

    Initial commit

---
+++ src/main/java/com/springboot/SpringBootReactiveWithGatlingApplication.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveWithGatlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveWithGatlingApplication.class, args);
+	}
+}
